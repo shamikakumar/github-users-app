7d65369f53511695fe9da608c58e3c1cba4d3a61
shamikum <shamikakumar1996@gmail.com>
2020-11-05 22:41:52 +0530

    added search and rate limit

--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,30 @@ const GithubProvider = ({children}) =>{
     const [githubUser,setGithubUser] = useState(mockUser);
     const [repos,setRepos] = useState(mockRepos);
     const [followers,setFollowers] = useState(mockFollowers);
+    const [requests, setRequests] = useState(0);
+    const [loading, setIsLoading] = useState(false);
+    const [error,setError] = useState({show:false,msg:""})
+
+    const checkRequests = () =>{
+        axios(`${rootUrl}/rate_limit`)
+            .then(({data})=>{
+                let {rate:{remaining}} = data;
+                setRequests(remaining)
+                if(remaining ===0)
+                {
+                    toggleError(true,'sorry, you have exceeded your hourly rate limit!')
+                }
+            })
+            .catch((err)=>console.log(err));
+    };
+
+    function toggleError(show = 'false',msg = ''){
+        setError({show,msg});
+    }
+
+    useEffect(checkRequests,[]);
     return (
-        <GithubContext.Provider value={{githubUser,repos,followers}}>{children}</GithubContext.Provider>
+        <GithubContext.Provider value={{githubUser,repos,followers, requests, error}}>{children}</GithubContext.Provider>
         );
 };
 
