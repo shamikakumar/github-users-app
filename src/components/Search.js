01e6d8950e882334f01981cd62d6e2ce37257611
shamikum <shamikakumar1996@gmail.com>
2020-11-06 14:37:21 +0530

    added search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,9 +4,13 @@ import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 const Search = () => {
   const [user,setUser] = React.useState('');
-  const {requests,error} = React.useContext(GithubContext);
+  const {requests,error, searchGithubUser, isLoading} = React.useContext(GithubContext);
   const handleSubmit = (e) => {
     e.preventDefault();
+    if(user)
+    {
+      searchGithubUser(user);
+    }
   }
   return (<section className="section">
     <Wrapper className="section-center">
@@ -19,7 +23,7 @@ const Search = () => {
         <div className="form-control">
           <MdSearch/>
           <input type='text' placeholder='enter github user' value={user} onChange={(e) =>setUser(e.target.value)} />
-          {requests>0 && <button type='submit'>search</button>}
+          {requests>0 && !isLoading && <button type='submit'>search</button>}
         </div>
       </form>
       <h3>requests : {requests} / 60</h3>
--- src/context/context.js
+++ src/context/context.js
@@ -13,8 +13,32 @@ const GithubProvider = ({children}) =>{
     const [repos,setRepos] = useState(mockRepos);
     const [followers,setFollowers] = useState(mockFollowers);
     const [requests, setRequests] = useState(0);
-    const [loading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
     const [error,setError] = useState({show:false,msg:""})
+
+    const searchGithubUser = async (user) => {
+        toggleError()
+        setIsLoading(true)
+        const response = await axios(`${rootUrl}/users/${user}`).
+        catch(err => console.log(error))
+        if(response){
+            setGithubUser(response.data)
+            const {login, followers_url} = response.data;
+            axios (`${rootUrl}/users/${login}/repos?per_page=100`).
+            then(response =>
+                setRepos(response.data)
+                );
+            axios (`${followers_url}?per_page=100`).
+            then(response =>
+                setFollowers(response.data)
+                );
+        }
+        else{
+            toggleError(true,'there is no user with that username')
+        }
+        checkRequests()
+        setIsLoading(false)
+    }
 
     const checkRequests = () =>{
         axios(`${rootUrl}/rate_limit`)
@@ -35,7 +59,7 @@ const GithubProvider = ({children}) =>{
 
     useEffect(checkRequests,[]);
     return (
-        <GithubContext.Provider value={{githubUser,repos,followers, requests, error}}>{children}</GithubContext.Provider>
+        <GithubContext.Provider value={{githubUser,repos,followers, requests, error, searchGithubUser, isLoading}}>{children}</GithubContext.Provider>
         );
 };
 
